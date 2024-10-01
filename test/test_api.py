9bdd266b8d857bf9c40a99633c94354d20da3317
Jane-Alex <ryadnova@gmail.com>
2024-10-01 12:22:21 +0300

    add changes

--- test/test_api.py
+++ test/test_api.py
@@ -1,14 +1,83 @@
 import requests
+import allure
 
 base_url = "https://api.kinopoisk.dev/v1.4"
 key = "FFVW3FK-F6PMTZW-G52F6R6-179PNR9"
 
-def search_movie_by_title():
+@allure.title("Поиск фильма по названию")
+@allure.description("Выполнение теста на поиск фильма по его названию")
+@allure.id(1)
+@allure.severity("blocker")
+def test_search_movie_by_title(): 
     headers = {
-    "X-API-KEY" : "key"
+    "X-API-KEY" : key
     }
-    response = requests.get(base_url+'/movie/search?page=1&limit=10&query=экипаж', headers=headers)
-    response_body = response.json()
-    first_movie = response_body[0]
-    assert first_movie ["name"] == "Экипаж"
-    assert response.status_code == 200
+    with allure.step("Отправляем GET запрос с названием фильма"):
+        response = requests.get(base_url+'/movie/search?page=1&limit=10&query=Экипаж', headers=headers)
+        response_body = response.json()
+        first_movie = response_body["docs"][0]['name']
+    with allure.step("Проверяем статус ответа"):
+        assert response.status_code == 200
+    with allure.step("Проверяем нахождение запрашиваемого фильма по названию"):
+        assert first_movie == "Экипаж"
+
+@allure.title("Поиск фильма по id")
+@allure.description("Выполнение теста на поиск фильма по его id")
+@allure.id(2)
+@allure.severity("blocker")
+def test_search_movie_by_id():
+    headers = {
+    "X-API-KEY" : key
+    }
+    with allure.step("Отправляем GET запрос с указанием id фильма"):
+        response = requests.get(base_url+'/movie/839818', headers=headers)
+        response_body = response.json()
+        id = response_body['id']
+    with allure.step("Проверяем статус ответа"):
+        assert response.status_code == 200
+    with allure.step("Проверяем нахождение запрашиваемого фильма по id"):
+        assert id == 839818
+
+@allure.title("Поиск фильма по фильтру ")
+@allure.description("Выполнение теста на поиск фильма по фильтру")
+@allure.id(3)
+@allure.severity("blocker")
+def test_search_with_filters():
+    headers = {
+    "X-API-KEY" : key
+    }
+    with allure.step("Отправляем GET запрос с заполненными полями в фильтре"):
+        response = requests.get(base_url+'/movie?page=1&limit=10&selectFields=names&type=movie&year=2016&genres.name=триллер&countries.name=Россия',
+                                 headers=headers)
+    with allure.step("Проверяем статус ответа"):
+        assert response.status_code == 200
+
+@allure.title("Поиск персоны по имени")
+@allure.description("Выполнение теста на поиск персоны по имени")
+@allure.id(4)
+@allure.severity("blocker")
+def test_search_person_by_name(): 
+    headers = {
+    "X-API-KEY" : key
+    }
+    with allure.step("Отправляем GET запрос с указанием имени персоны"):
+        response = requests.get(base_url+'/person/search?page=1&limit=10&query=Данила Козловский', headers=headers)
+        response_body = response.json()
+        first_movie = response_body["docs"][0]['name']
+    with allure.step("Проверяем статус ответа"):
+        assert response.status_code == 200
+    with allure.step("Проверяем нахождение запрашиваемой персоны по имени"):
+        assert first_movie == "Данила Козловский"
+
+@allure.title("Получение рандомного тайтла")
+@allure.description("Выполнение теста на получение рандомного тайтла из базы")
+@allure.id(5)
+@allure.severity("blocker")
+def test_get_random_title():
+    headers = {
+    "X-API-KEY" : key
+    }
+    with allure.step("Отправляем GET запрос на получение рандомного тайтла"):
+        response = requests.get(base_url+'/movie/random?notNullFields=id&notNullFields=name', headers=headers)
+    with allure.step("Проверяем статус ответа"):
+        assert response.status_code == 200
--- test/test_ui.py
+++ test/test_ui.py
@@ -4,7 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import allure 
 import pytest
-from time import sleep
 
 @pytest.fixture 
 def browser(): 
@@ -15,10 +14,11 @@ def browser():
     browser.quit()
 
 
-#@allure.title("Поиск фильма по названию на кириллице") 
-#@allure.description("Тест проверяет поиск фильма на кириллице") 
-#@allure.severity("blocker")
-def test_find_film(browser):
+@allure.title("Поиск фильма по названию на кириллице") 
+@allure.description("Тест проверяет поиск фильма на кириллице")
+@allure.id(1) 
+@allure.severity("blocker")
+def test_find_film_rus(browser):
     browser.get("https://www.kinopoisk.ru/")
     with allure.step("Добавлены ожидания и открытие окна"): 
         browser.implicitly_wait(10)
@@ -28,7 +28,70 @@ def test_find_film(browser):
         browser.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
     with allure.step("Проверяем результат по запросу"): 
         result = browser.find_element(By.CSS_SELECTOR, 'a[data-type="film"]').text
-        assert result == "экипаж"
-        #assert "" in browser.find_element(By.CSS_SELECTOR, 'p[class="name"]').text
+        assert result, "экипаж"
 
+@allure.title("Поиск фильма по названию на латинице") 
+@allure.description("Тест проверяет поиск фильма на латинице")
+@allure.id(2) 
+@allure.severity("blocker")
+def test_find_film_lat(browser):
+    browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Добавлены ожидания и открытие окна"): 
+        browser.implicitly_wait(10)
+        browser.maximize_window()
+    with allure.step("Выполнение поиска фильма"): 
+        browser.find_element(By.CSS_SELECTOR, "input[name=kp_query]").send_keys("Titanic")
+        browser.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
+    with allure.step("Проверяем результат по запросу"): 
+        result = browser.find_element(By.CSS_SELECTOR, 'span[class="gray"]').text
+        assert result, "Titanic"
+
+@allure.title("Поиск актера по имени") 
+@allure.description("Тест проверяет поиск актера по имени")
+@allure.id(3) 
+@allure.severity("blocker")
+def test_find_actor(browser):
+    browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Добавлены ожидания и открытие окна"): 
+        browser.implicitly_wait(10)
+        browser.maximize_window()
+    with allure.step("Выполнение поиска фильма"): 
+        browser.find_element(By.CSS_SELECTOR, "input[name=kp_query]").send_keys("ДиКаприо")
+        browser.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
+    with allure.step("Проверяем результат по запросу"): 
+        result = browser.find_element(By.CSS_SELECTOR, 'a[data-type="person"]').text
+        assert result, "ДиКаприо"
+
+@allure.title("Расширенный поиск фильмов по году")  
+@allure.description("Тест проверяет поиск фильмов по году")
+@allure.id(4) 
+@allure.severity("blocker")
+def test_find_film_by_id(browser):
+    browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Добавлены ожидания и открытие окна"): 
+        browser.implicitly_wait(10)
+        browser.maximize_window()
+    with allure.step("Открываем вкладку 'Расширенный поиск"): 
+        browser.find_element(By.CSS_SELECTOR, 'a[aria-label="Расширенный поиск"]').click()
+    with allure.step("Выполнение поиска фильма по году"):
+        browser.find_element(By.CSS_SELECTOR, "#year").send_keys("2016")
+        browser.find_element(By.CSS_SELECTOR, "input[type=button]").click()
+    with allure.step("Проверяем результат по запросу"): 
+        result = browser.find_element(By.CSS_SELECTOR, 'span[class="year"]').text
+        assert result == "2016"
         
+@allure.title("Поиск фильма по цифре") 
+@allure.description("Тест проверяет поиск фильма при вводе цифры в поле поиска")
+@allure.id(5) 
+@allure.severity("blocker")
+def test_find_film_figure(browser):
+    browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Добавлены ожидания и открытие окна"): 
+        browser.implicitly_wait(10)
+        browser.maximize_window()
+    with allure.step("Выполнение поиска фильма"): 
+        browser.find_element(By.CSS_SELECTOR, "input[name=kp_query]").send_keys("7")
+        browser.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
+    with allure.step("Проверяем результат по запросу"): 
+        result = browser.find_element(By.CSS_SELECTOR, 'a[data-type="film"]').text
+        assert result, "7"
